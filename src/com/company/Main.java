50daaf21e360cd95e6022327105090c95bf46808
Nursultan-oss <tashtanbiekov@bk.ru>
2022-03-16 15:15:42 +0600

    initial commit second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Boss boss = new Boss(750, 70,new Weapon("ColdWeapon","Knife"));
-        System.out.println("Boss" + boss.printInfo());
+
         Skeleton skeleton1= new Skeleton(100,5,new Weapon("gunshot", "Gun"));
-        System.out.println( "Skeleton 1" + skeleton1.printInfo());
         Skeleton skeleton2= new Skeleton(103,6,new Weapon("gunshot", "desert eagle"));
-        System.out.println( "Skeleton 2" + skeleton2.printInfo());
+
+        System.out.println("Boss" + boss.printInfo()  + "\nSkeleton 1" + skeleton1.printInfo() + "\nSkeleton 2" + skeleton2.printInfo());
 
     }
 }
